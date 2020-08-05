96b5f43272f7c6f8f51713e8d0dd60e3d39f6f47
Sino Plenis <plenissino@gmail.com>
2020-08-05 14:03:07 +0200

    added radio buttons for languages, return message and counter for all names

--- src/main/java/net/myfirst/webapp/App.java
+++ src/main/java/net/myfirst/webapp/App.java
@@ -2,67 +2,56 @@ package net.myfirst.webapp;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.*;
 
 public class App {
 
-    public static void main(String[] args) {
+    static Map<String, Object> map = new HashMap<>();
+    static List<String> users = new ArrayList<>();
+
+        public static void main(String[] args) {
         staticFiles.location("public");
-        Map<String, Object> map = new HashMap<>();
 
-        get("/hello", (request, response) ->{
-
-//            Map<String, Object> map = new HashMap<>();
-            return new ModelAndView(map, "hello.handlebars");
-
-        }, new HandlebarsTemplateEngine());
+        get("/hello", (request, response) -> new ModelAndView(map, "hello.handlebars"), new HandlebarsTemplateEngine());
 
 
         post("/hello", (request, response) -> {
+
             // create the greeting message
-            String greeting = "Hello, " + request.queryParams("username");
-            String[] users = request.queryParamsValues("username");
+            String user = request.queryParams("username");
+            String lang = request.queryParams("language");
+
+            if(!users.contains(user)){
+                users.add(user);
+            }
+
+            String greet = "";
+
+                if (lang.equals("IsiXhosa")) {
+                    greet = "Molo " + user;
+                } else if (lang.equals("English")) {
+                    greet = "Hello " + user;
+                } else if (lang.equals("Afrikaans")) {
+                    greet = "Hallo " + user;
+                }
+
+
+            String greeting = greet;
+            int counter = users.size();
 
             // put it in the map which is passed to the template - the value will be merged into the template
             map.put("greeting", greeting);
             map.put("users", users);
+            map.put("counter", counter);
             return new ModelAndView(map, "hello.handlebars");
 
         }, new HandlebarsTemplateEngine());
 
-//        get("hello", (request, response) ->{
-//
-////            Map<String, Object> map = new HashMap<>();
-//            return map;
-//
-//        });
-//
-//        post("hello", (request, response) ->{
-//
-////            Map<String, Object> map = new HashMap<>();
-//
-//            String users = request.queryParams(":greeted");
-//
-//            map.put("users", users);
-//            return map;
-//        });
-
-//        get("/greet", (request, response) ->
-//                "Molo!");
-//
-//        post("/greet", (request, response) ->
-//                "Molo " + request.queryParams("username")
-//        );
-//
-//        get("/greet/:username", (request, response) ->
-//                "Molo " + request.params("username"));
-//
-//        get("/greet/:username/language/:language", ((request, response) ->
-//                "Greet user " + request.params("username") + " in " + request.params("language")
-//        ));
     }
 
 }
